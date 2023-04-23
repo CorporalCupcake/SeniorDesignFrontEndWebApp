55c1527f434f3049b106381708efebe1815e8717
Ameen's MBP <ayubameen1@gmail.com>
2023-04-23 18:31:24 +0300

    .

--- src/pages/SandboxPage.jsx
+++ src/pages/SandboxPage.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 
-import Button from "../components/button/button.component";
 
 const SandboxPage = () => (
-    <>
-        <Button label='CLICK ME'/>
-    </>
+<></>
 )
 
 export default SandboxPage;
