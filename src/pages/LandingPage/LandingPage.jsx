1ab383ed40b2d11dc3e7daf05a949bcbe8f706cc
Ameen's MBP <ayubameen1@gmail.com>
2023-05-09 10:41:38 +0400

    .

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -10,8 +10,6 @@ const LandingPage = () => {
     return (
         <div className="landing-page">
             <h1>Welcome to our B2C Food Delivery Driver Managment Platform</h1>
-            <p>Manage Driver Behaviour</p>
-            <button className="get-started-button">Get Started</button>
             <IconContext.Provider value={{ color: 'white', size: '100px' }}>
                 <RiMotorbikeFill className='bike_icon'/>
             </IconContext.Provider>
